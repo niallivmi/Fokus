9d07c363a3216aa5ae51228fd71904574f712bd7
niallivmi <majucavalcantix@gmail.com>
2025-11-20 18:18:20 -0300

    Separar em funções.

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
+
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', '/imagens/foco.png')
+    alterarContexto('foco')
 })
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+   alterarContexto('descanso-curto')
 })
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
 })
