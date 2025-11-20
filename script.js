cb5b4c6a10555331fefe09c28aee89885ec87d00
niallivmi <majucavalcantix@gmail.com>
2025-11-20 18:02:59 -0300

    Corrigir sintaxe de classe, faltou '.'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('app__card-button--curto')
-const longoBt = document.querySelector('app__card-button--longo')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
